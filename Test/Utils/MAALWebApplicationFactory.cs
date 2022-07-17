59294b1698533d02deb9e89630ec39b6aa838f5d
Mathias Løken <lakavi@mailfence.com>
2022-07-17 14:43:12 +0200

    Fix tests

--- Test/Utils/MAALWebApplicationFactory.cs
+++ Test/Utils/MAALWebApplicationFactory.cs
@@ -18,7 +18,19 @@ public sealed class MAALWebApplicationFactory<TStartup> : WebApplicationFactory<
 				new KeyValuePair<string, string>("Authentication:Google:ClientId", "null"),
 				new KeyValuePair<string, string>("Authentication:Google:ClientSecret", "null"),
 				new KeyValuePair<string, string>("Authentication:Twitter:ClientId", "null"),
-				new KeyValuePair<string, string>("Authentication:Twitter:ClientSecret", "null")
+				new KeyValuePair<string, string>("Authentication:Twitter:ClientSecret", "null"),
+				new KeyValuePair<string, string>("Authentication:GitHub:ClientId", "null"),
+				new KeyValuePair<string, string>("Authentication:GitHub:ClientSecret", "null"),
+				new KeyValuePair<string, string>("Authentication:Microsoft:ClientId", "null"),
+				new KeyValuePair<string, string>("Authentication:Microsoft:ClientSecret", "null"),
+				new KeyValuePair<string, string>("Authentication:LinkedIn:ClientId", "null"),
+				new KeyValuePair<string, string>("Authentication:LinkedIn:ClientSecret", "null"),
+				new KeyValuePair<string, string>("Authentication:MAAL:ClientId", "null"),
+				new KeyValuePair<string, string>("Authentication:MAAL:ClientSecret", "null"),
+				new KeyValuePair<string, string>("Authentication:Pinterest:ClientId", "null"),
+				new KeyValuePair<string, string>("Authentication:Pinterest:ClientSecret", "null"),
+				new KeyValuePair<string, string>("Authentication:Facebook:ClientId", "null"),
+				new KeyValuePair<string, string>("Authentication:Facebook:ClientSecret", "null")
 			})
 			.Build();
 
