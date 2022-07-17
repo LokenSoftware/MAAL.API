cd1e7c7d2e56b48d79ee56d7ec635198855a8354
Mathias Løken <lakavi@mailfence.com>
2022-07-17 16:19:03 +0200

    Order alphabetically

--- Source/Enums/IdentityProvider.cs
+++ Source/Enums/IdentityProvider.cs
@@ -1,26 +1,32 @@
-﻿namespace MAAL.API.Enums;
+﻿using JetBrains.Annotations;
+
+namespace MAAL.API.Enums;
 
 /// <summary> Supported identity providers </summary>
+[PublicAPI]
 public enum IdentityProvider
 {
-	/// <summary> Microsoft account </summary>
-	Microsoft,
-
-	/// <summary> Google account </summary>
-	Google,
-
-	/// <summary> Twitter account </summary>
-	Twitter,
-
-	/// <summary> MAAL account </summary>
-	MAAL,
+	/// <summary> Facebook account </summary>
+	Facebook,
 
 	/// <summary> GitHub account </summary>
 	GitHub,
 
+	/// <summary> Google account </summary>
+	Google,
+
+	/// <summary> LinkedIn account </summary>
+	LinkedIn,
+
+	/// <summary> MAAL account </summary>
+	MAAL,
+
+	/// <summary> Microsoft account </summary>
+	Microsoft,
+
 	/// <summary> Pinterest account </summary>
 	Pinterest,
 
-	/// <summary> Facebook account </summary>
-	Facebook
+	/// <summary> Twitter account </summary>
+	Twitter
 }
