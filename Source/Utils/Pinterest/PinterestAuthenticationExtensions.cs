5ba0150fca86a80a224a92e3c48c1449beda5363
Mathias Løken <77194821+MathiasFrost@users.noreply.github.com>
2022-07-19 17:10:09 +0200

    Pinterest support
    * Pinterest support
    
    * Note

---
+++ Source/Utils/Pinterest/PinterestAuthenticationExtensions.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.Annotations;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
+namespace MAAL.API.Utils.Pinterest;
+
+/// <summary> </summary>
+internal static class PinterestAuthenticationExtensions
+{
+	/// <summary> </summary>
+	[PublicAPI]
+	internal static AuthenticationBuilder AddPinterest(this AuthenticationBuilder builder,
+	                                                   Action<PinterestAuthenticationOptions> configuration)
+	{
+		builder.Services.TryAddSingleton<IPostConfigureOptions<PinterestAuthenticationOptions>, PinterestPostConfigureOptions>();
+		return builder.AddOAuth<PinterestAuthenticationOptions, PinterestAuthenticationHandler>("Pinterest",
+			"Pinterest",
+			configuration);
+	}
+}
---
+++ Source/Utils/Pinterest/PinterestAuthenticationOptions.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+
+namespace MAAL.API.Utils.Pinterest;
+
+/// <inheritdoc />
+internal sealed class PinterestAuthenticationOptions : OAuthOptions
+{
+	/// <inheritdoc />
+	public PinterestAuthenticationOptions()
+	{
+		ClaimsIssuer = PinterestAuthenticationDefaults.Issuer;
+		CallbackPath = PinterestAuthenticationDefaults.CallbackPath;
+
+		AuthorizationEndpoint = PinterestAuthenticationDefaults.AuthorizationEndpoint;
+		TokenEndpoint = PinterestAuthenticationDefaults.TokenEndpoint;
+		UserInformationEndpoint = PinterestAuthenticationDefaults.UserInformationEndpoint;
+
+		Scope.Add("user_accounts:read");
+		
+		// NOTE: NameIdentifier should be PK. Maybe username is PK in Pinterest's DB, but that would be unusual...
+		ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "username");
+		ClaimActions.MapJsonKey(ClaimTypes.Name, "username");
+	}
+}
